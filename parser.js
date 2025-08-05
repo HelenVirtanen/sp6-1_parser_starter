d42b1e0aaaa89fb0f5b95203adcda873a3749558
Helen Virtanen <helenvirtanen28@gmail.com>
2025-08-05 20:21:45 +0300

    Add 4 common get data methods and getMetaContent method

--- parser.js
+++ parser.js
@@ -1,12 +1,43 @@
 // @todo: напишите здесь код парсера
-
-function parsePage() {
-    return {
-        meta: {},
-        product: {},
-        suggested: [],
-        reviews: []
-    };
+function getMetaContent(name) {
+    let metaAttribute = document.querySelector(`meta[name="${name}"]`);
+    return metaAttribute.getAttribute('content').trim();
 }
 
-window.parsePage = parsePage;
+function getMeta() {
+    const meta = {};
+    
+    meta.title = document.querySelector("h1.title").textContent;
+    meta.description = getMetaContent('description');
+    meta.keywords = getMetaContent('keywords').split(",");
+
+    meta.language = document.documentElement.lang;
+
+    console.log(meta);
+    return meta
+}
+
+getMeta();
+
+// function getProduct() {
+
+// }
+
+// function getSuggested() {
+
+// }
+
+// function getReviews() {
+
+// }
+
+// function parsePage() {
+//     return {
+//         meta: {},
+//         product: {},
+//         suggested: [],
+//         reviews: []
+//     };
+// }
+
+// window.parsePage = parsePage;
